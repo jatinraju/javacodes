045d0fff0bb2b1f078992ffa05f411d51ce38c2a
jatinraju <100117446+jatinraju@users.noreply.github.com>
2024-01-20 13:36:11 +0530

    spring security no db

---
+++ SprinSecurityNoDB/src/main/java/com/eg/SprinSecurityNoDbApplication.java
@@ -0,0 +1,13 @@
+package com.eg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinSecurityNoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinSecurityNoDbApplication.class, args);
+	}
+
+}
---
+++ SprinSecurityNoDB/src/main/java/com/eg/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.eg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests().requestMatchers("/public/hello").permitAll()
+				.anyRequest().authenticated().and().formLogin();
+
+		return httpSecurity.build();
+	}
+
+}
