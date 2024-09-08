b0d9514b4fc3293110e9595f76ea17d4fad36bb9
jatinraju <100117446+jatinraju@users.noreply.github.com>
2024-09-08 16:46:44 +0530

    Update SecurityConfig.java with latest version

--- SprinSecurityNoDB/src/main/java/com/eg/config/SecurityConfig.java
+++ SprinSecurityNoDB/src/main/java/com/eg/config/SecurityConfig.java
@@ -2,8 +2,15 @@ package com.eg.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,15 +18,30 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//		httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests().requestMatchers("/public/hello").permitAll()
-//				.anyRequest().authenticated().and().formLogin();
-
-		httpSecurity.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/public/hello").permitAll().anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults());
-
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity.csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll() // Allow public APIs
+																								// without
+																								// authentication
+						.requestMatchers("/admin/**").hasRole("ADMIN") // Admin role required for /admin/**
+						.requestMatchers("/user/**").hasRole("USER") // User role required for /normal/**
+						.anyRequest().authenticated() // Any other request requires authentication
+				).httpBasic(Customizer.withDefaults()) // Enable HTTP Basic Authentication
+				.build();
 	}
 
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user").password(passwordEncoder().encode("password")).roles("USER")
+				.build();
+		UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
